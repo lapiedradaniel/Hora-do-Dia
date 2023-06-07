16d0878e9991d92a5e65aad48166a3d3e3fc15be
Daniel <danielsantosaraujo2010@hotmail.com>
2023-06-07 12:48:55 -0300

    final

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >=0 && hora < 12) {
+    img.src = 'imagens/manha.jpg'
+    document.body.style.background = '#e2cd9f'
+}else if (hora >= 12 && hora < 18){
+    img.src = 'imagens/tarde.jpg'
+    document.body.style.background = 'rgba(231, 192, 123, 0.918)'
+}else {
+    img.src = 'imagens/noite.jpg'
+    document.body.style.background = '#515154'
+}
+
+}
